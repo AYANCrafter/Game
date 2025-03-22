58f4ca9a5f70cc3c63ada91f6d0f0383a778ffb2
Ayan Sarkar <ayansarkar4005@gmail.com>
2025-03-22 21:00:45 +0530

    main first commit

--- first.js
+++ first.js
@@ -6,12 +6,14 @@ const game="Football";
 const player="CR7";
 const team="Portugal";
 
-<<<<<<< HEAD
+
 const game2="Cricket";
 const player2="Virat Kohli";
 const team2="Bangalore";
-=======
+
 const game1="Football";
 const player1="Messi";
 const team1="Argentina";
->>>>>>> 3a1a97a (rivalry first commit)
+
+const School="D.B.M.S English School";
+const board="ISC";
